8c22cc6b7f798ddda21b37b0cd3317580ad2453e
Wolfgang Wallner <wolfgang-wallner@gmx.at>
2015-03-14 00:46:38 +0100

    Demo: add RW chain

--- Examples/AverageOscillator_20MHz/RW_Chain.cpp
+++ Examples/AverageOscillator_20MHz/RW_Chain.cpp
@@ -29,6 +29,36 @@ namespace AverageOscillator_20MHz
 
 TdOracle_AvgOsc20MHz::TdEstChain_RW::TdEstChain_RW( unsigned int Seed )
 {
+    TdEstimatorConfig   Conf_100Hz;
+
+    // -------------------------------------------------------------------------------------------
+    // Configure 100 Hz
+    // -------------------------------------------------------------------------------------------
+    Conf_100Hz.SampleConf.f_s               = 200;
+    Conf_100Hz.SampleConf.TdVecLen          = 5100;
+
+    Conf_100Hz.KwImplOption                 = USE_SHORTCUTS;
+
+    Conf_100Hz.KwConf.Qd                    = 175E-32;
+    Conf_100Hz.KwConf.alpha                 = -2.0;
+    Conf_100Hz.KwConf.FilterLen             = 10;
+    Conf_100Hz.KwConf.Seed                  = Seed + 0;
+
+    Conf_100Hz.HpConf.Type                  = NO_FILTER;
+    Conf_100Hz.HpConf.f_c_nom               = 0.01;
+    Conf_100Hz.HpConf.FilterLen             = 5001;
+    Conf_100Hz.HpConf.Cnt                   = 1;
+
+    Conf_100Hz.InterpolConf.Type            = CUBIC_SPLINE_INTERPOLATION;
+
+    Conf_100Hz.TimeConf.T_val               = 2000;
+    Conf_100Hz.TimeConf.ForgetTh1           = 5;
+    Conf_100Hz.TimeConf.ForgetTh2           = 2;
+
+    // -------------------------------------------------------------------------------------------
+    // Combine
+    // -------------------------------------------------------------------------------------------
+    AddTdEstimator( Conf_100Hz, 1.0L );
 
 }
 
