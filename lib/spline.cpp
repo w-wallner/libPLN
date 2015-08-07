b0fa5f1ffe15fe55e7289503889de261636fd31f
Wolfgang Wallner <wolfgang-wallner@gmx.at>
2015-08-08 00:03:41 +0200

    License: Fix wrong license for spline files.

--- lib/spline.cpp
+++ lib/spline.cpp
@@ -1,21 +1,27 @@
-// 
-// Copyright 2015 Wolfgang Wallner (wolfgang-wallner AT gmx.at)
-// 
-// This file is part of libPLN.
-// 
-// libPLN is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// libPLN is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public License
-// along with libPLN.  If not, see <http://www.gnu.org/licenses/>.
-//
+/*
+ * spline.cpp
+ *
+ * simple cubic spline interpolation library without external
+ * dependencies
+ *
+ * ---------------------------------------------------------------------
+ * Copyright (C) 2011, 2014 Tino Kluge (ttk448 at gmail.com)
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * ---------------------------------------------------------------------
+ *
+ */
 
 // =========================================================================
 // Includes
