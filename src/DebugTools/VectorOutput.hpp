20b206f295bb4f0d5ff5979a1ebf038468a5f904
Wolfgang Wallner <wolfgang-wallner@gmx.at>
2015-09-04 13:56:27 +0200

    DebugTools: remove unused stream operator.

--- src/DebugTools/VectorOutput.hpp
+++ src/DebugTools/VectorOutput.hpp
@@ -58,19 +58,6 @@ using namespace std;
 // Function declarations
 // =========================================================================
 
-template<typename T>
-ostream& operator<< (ostream& out, const vector<T>& v) {
-    out << "[";
-    size_t last = v.size() - 1;
-    for(size_t i = 0; i < v.size(); ++i) {
-        out << v[i];
-        if (i != last)
-            out << ", ";
-    }
-    out << "]";
-    return out;
-}
-
 void    PrintRealVector( std::string Name, std::vector<double> v );
 void    PrintCompVector( std::string Name, std::vector< std::complex<double> > v );
 void    PrintFftRealVector( std::string Name, FFT_RealVector v );
