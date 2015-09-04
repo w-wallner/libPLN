62701ed32f9de1f377caec58c3f19732251c00aa
Wolfgang Wallner <wolfgang-wallner@gmx.at>
2015-09-04 17:27:13 +0200

    Doc/TdVecGen: add doxygen info

--- src/TdVecGen/TdVecGen.hpp
+++ src/TdVecGen/TdVecGen.hpp
@@ -52,54 +52,100 @@
 // Type definitions
 // =========================================================================
 
+/// Generator for TdVectors
 class TdVecGen
 {
     protected:
 
+        // -----------------------------------------------------------------
         // Types
+        // -----------------------------------------------------------------
+
+
+        /// Enumeration to encode the state of internal convolution
+        ///
+        /// The time deviation vector generator works by overlapping convolution.
+        /// Thus, each new vector depends on the last one.
+        /// This means we need to know if we have already generated a vector
+        /// before or not.
         typedef enum
         {
-            UNINITIALIZED,
-            INITIALIZED,
+            UNINITIALIZED,      ///< Uninitialized: We can't base the vector on an existing one.
+            INITIALIZED,        ///< Initialized    We already have data from an existing old vector.
         }
-        TdGenState;
+        ConvState_t;
 
+        // -----------------------------------------------------------------
         // Config
-        double              TickLen;
-        InterpolationType   IntpolType;
-        size_t              TdVecLen;
+        // -----------------------------------------------------------------
+        double              TickLen;        ///< Time (in seconds) between two simulated TD values
+        InterpolationType   IntpolType;     ///< Interpolation type for the generated TD vectors
+        size_t              TdVecLen;       ///< Length of the generated TD vectors
 
+        // -----------------------------------------------------------------
         // House keeping
-        TdGenState          ConvState;
-        double              Last_t_end;
-        double              Last_TD_nom;
-        size_t              FfdVecLen;
+        // -----------------------------------------------------------------
+        ConvState_t         ConvState;      ///< Current state of the convolution
+        double              Last_t_end;     ///< Time (in seconds) at the end of the last generated TD vector
+        double              Last_TD_nom;    ///< Time deviation (in seconds) at the end of the last generated vector
+        size_t              FfdVecLen;      ///< Length of the Fractional Frequency Deviation vectors that are used to generate Time Deviation vectors
 
+        // -----------------------------------------------------------------
         // Resources
-        WhiteNoiseGenerator     WhiteNoiseGen;
-        FilterKernel            H;
-        FFT_RealVector          *pLastFFD;
+        // -----------------------------------------------------------------
+        WhiteNoiseGenerator     WhiteNoiseGen;      ///< White noise generator
+        FilterKernel            H;                  ///< Filter kernel to shape the white noise
+        FFT_RealVector          *pLastFFD;          ///< Pointer to the laste generated Fractional Frequency Deviation vector
 
+        // -----------------------------------------------------------------
         // Internal functions
+        // -----------------------------------------------------------------
+
+        /// Initialize the convolution filter
         void        InitConvFilter();
+
+        /// Apply the convolution filter to a data vector
+        ///
+        /// \param pw   Data vector that should be filtered
         void        ApplyConvFilter( FFT_RealVector *pw );
+
+        /// Reset the convoultion filter
         void        ResetConvFilter();
 
+        /// Construct a Time Deviation vector from given input data
+        ///
+        /// Depending on the type of input data different operations are performed:
+        /// - FFD data: FFD -> TD conversion is done
+        /// - TD data:  no conversion needed
+        /// \param
         TdVector    *ConstructTdVector( FFT_RealVector *pData, TdVector::TdVecDataType DataType );
+
+        // -----------------------------------------------------------------
+        // Constructors
+        // -----------------------------------------------------------------
+
+        /// Constructor
+        ///
+        /// \param
+        TdVecGen( size_t TdVecLen, double TickLen, KW_FilterConfig KwConf, InterpolationConfig InterpolConf );
+        TdVecGen( const TdVecGen& other );
 
     public:
 
+        // -----------------------------------------------------------------
         // Constructors/Destructor
-        TdVecGen( size_t TdVecLen, double TickLen, KW_FilterConfig KwConf, InterpolationConfig InterpolConf );
-        TdVecGen( const TdVecGen& other );
+        // -----------------------------------------------------------------
         virtual ~TdVecGen();
-
         virtual TdVecGen* Clone() const = 0;  // Virtual constructor (copying)
 
+        // -----------------------------------------------------------------
         // Operators
+        // -----------------------------------------------------------------
         TdVecGen&  operator=( const TdVecGen& other );
 
+        // -----------------------------------------------------------------
         // API
+        // -----------------------------------------------------------------
         virtual void        ResetToFixPoint( TdFixPoint fp );
         virtual void        SetSeed( unsigned int Seed );
         virtual TdVector    *GetNextVector() = 0;
