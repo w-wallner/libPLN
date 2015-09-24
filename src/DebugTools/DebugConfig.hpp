d936b4e0e11ffb104ede51ddf32184743947f956
Wolfgang Wallner <wolfgang-wallner@gmx.at>
2015-09-24 17:13:47 +0200

    DebugConfig: add files

---
+++ src/DebugTools/DebugConfig.hpp
@@ -0,0 +1,68 @@
+// ============================================================================
+///
+/// \file
+///
+/// \brief
+///
+/// \ingroup
+///
+// ============================================================================
+
+// ============================================================================
+// 
+// Copyright 2015 Wolfgang Wallner (wolfgang-wallner AT gmx.at)
+// 
+// This file is part of libPLN.
+// 
+// libPLN is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License.com as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// libPLN is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License.com for more details.
+// 
+// You should have received a copy of the GNU General Public License.com
+// along with libPLN.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ============================================================================
+
+#ifndef DEBUG_CONFIG_HPP_
+#define DEBUG_CONFIG_HPP_
+
+// =========================================================================
+// Includes
+// =========================================================================
+
+#include <string>
+
+// =========================================================================
+// Defines
+// =========================================================================
+
+// =========================================================================
+// Type definitions
+// =========================================================================
+
+typedef struct
+{
+    bool            VerboseTdVecGen;
+}
+DebugConfig_t;
+
+// =========================================================================
+// Global variables
+// =========================================================================
+
+extern  const   DebugConfig_t   AllDisabledDebugConfig;
+extern  const   DebugConfig_t   AllEnabledDebugConfig;
+
+extern DebugConfig_t   DebugConfig;
+
+// =========================================================================
+// Function declarations
+// =========================================================================
+
+#endif
